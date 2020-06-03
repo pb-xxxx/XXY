3031d42e8f842248338581dc14605c38ccb6aaea
realcwt <2420978123@qq.com>
2020-06-03 16:26:15 +0800

    上传代码

---
+++ javaapkcomcampus_life/build/generated/source/buildConfig/androidTest/debug/com/yhx/app/campus_life/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.yhx.app.campus_life.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.yhx.app.campus_life.test";
+  public static final String BUILD_TYPE = "debug";
+}
